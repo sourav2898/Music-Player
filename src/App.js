b278e91fcc7bf12668a57040a10ecb9d337b3301
Sourav Kumar <souravkumardubey2898@gmail.com>
2020-12-14 23:49:13 +0500

    radio done without music bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Navbar from './Navbar';
+import Radio from './Radio';
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Switch>
-        <Route />
+        <Route exact path="/" component={Radio} />
       </Switch>
     </>
   )
--- src/Radio.jsx
+++ src/Radio.jsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Radio = () => {
+
     return (
         <>
-            
+            <div className="radio">
+                <div className="header">
+                    <h1>Radio</h1>
+                </div>
+                <div className="container">
+                    <div className="card">
+                        <div className="card-title" style={{ backgroundColor: "#4C4CAB" }}>
+                            <NavLink exact to="/" className="h3"><h3>Radio</h3> </NavLink>
+                        </div>
+                        <div className="subtitle">
+                            <h3>Home Radio</h3>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-title" style={{ backgroundColor: "#FF808B" }}>
+                            <NavLink exact to="/" className="h3"><h3>Radio</h3> </NavLink>
+                        </div>
+                        <div className="subtitle">
+                            <h3>Broadcast Radio</h3>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-title" style={{ backgroundColor: "#4C4CAB" }}>
+                            <NavLink exact to="/" className="h3"><h3>Radio</h3> </NavLink>
+                        </div>
+                        <div className="subtitle">
+                            <h3>Space Radio</h3>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-title" style={{ backgroundColor: "#131336" }}>
+                            <NavLink exact to="/" className="h3"><h3>Radio</h3> </NavLink>
+                        </div>
+                        <div className="subtitle">
+                            <h3>Home Radio</h3>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-title" style={{ backgroundColor: "#42425E" }}>
+                            <NavLink exact to="/" className="h3"><h3>Radio</h3> </NavLink>
+                        </div>
+                        <div className="subtitle">
+                            <h3>Broadcast Radio</h3>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <div className="card-title" >
+                            <h3></h3>
+                        </div>
+                        <div className="">
+                            <h3></h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
